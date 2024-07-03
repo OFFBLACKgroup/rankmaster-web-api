23ad31eb6e4b19aea78d2e11d63caca3a18fe400
OFFBLACKgroup <offblack.group@gmail.com>
2024-07-03 13:54:36 +0200

    first commit

---
+++ functions.js
@@ -0,0 +1,38 @@
+import FormData from 'form-data';
+import fetch from 'node-fetch';
+import 'dotenv/config'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+
+export async function sendEmail(email) {
+  const form = new FormData();
+  form.append('from','RankMaster Support<support@rankmaster.click>');
+  form.append('to',`${email}`);
+  form.append('subject','RankMaster Waiting List');
+  form.append('template', 'rankmaster');
+
+  const domainName = 'mg.rankmaster.click';
+
+  const resp = await fetch(
+    `https://api.mailgun.net/v3/${domainName}/messages`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: 'Basic ' + Buffer.from(`api:${process.env.MAILGUN_KEY}`).toString('base64')
+      },
+      body: form
+    }
+  );
+  const data = await resp.text()
+  console.log(data);
+}
+
+export async function uploadEmail(email) {
+  const { error } = await supabase
+  .from('emails')
+  .insert({ email: email })
+  if (error) {
+    console.log(error)
+  }
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors'
+import { sendEmail, uploadEmail } from './functions.js';
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors({ origin: 'http://localhost:4200' }))
+
+app.post('/send', (req, res) => {
+  try {
+    // sendEmail(req.body.email)
+    uploadEmail(req.body.email)
+    res.sendStatus(200)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(404)
+  }
+});
+
+app.get('/test', (req, res) => {
+  res.send('Hello')
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);  
+});
+
