722b28a4c2c8e6398b6330f39fff59d75f214be6
OFFBLACKgroup <offblack.group@gmail.com>
2024-08-03 14:00:44 +0200

    testing error v4b

--- api/functions.js
+++ api/functions.js
@@ -191,7 +191,7 @@ async function createUserLog(completedTierlists) {
   .select()
 
   if (error) {
-    throw new Error(`Went wrong here. Completed_tierlist_logs: ${completedTierlists}. Error: ${error}`)
+    throw new Error('Went wrong here. Completed_tierlist_logs: ' + completedTierlists + 'Error: ' + error)
   } else return 
 }
 
