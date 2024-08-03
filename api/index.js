6a312be045e31e13a7fcb4f3fb0b9d4dff124f22
OFFBLACKgroup <offblack.group@gmail.com>
2024-08-03 17:01:22 +0200

    fixing signUp params v2

--- api/index.js
+++ api/index.js
@@ -66,7 +66,7 @@ app.get('/fetchTierlist/:id', async (req, res) => {
 
 app.post('/signUp', async (req, res) => {
   try {
-    const data = await signUp(req.body.email, req.body.password, res.anon)
+    const data = await signUp(req.body.email, req.body.password, res.body.anon)
     res.json(data)
   } catch (error) {
     console.log(error)
