bdbadf10fb8e5e8c58f1b3e28914b229c0a87109
OFFBLACKgroup <offblack.group@gmail.com>
2024-08-05 14:20:03 +0200

    setting additional headers v2

--- api/index.js
+++ api/index.js
@@ -150,6 +150,7 @@ app.get('/leaderboard', async (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
 
     const session = await betterSSE.createSession(req, res)
 
