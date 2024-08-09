9715e3c856cdd9e78ad3bfd2e5f5dfb43d1f9dea
OFFBLACKgroup <offblack.group@gmail.com>
2024-08-09 03:37:51 +0200

    fixed points being updated to total

--- api/functions.js
+++ api/functions.js
@@ -396,7 +396,7 @@ export async function updateLeaderboard(points) {
 
   const { data: data2, error: error2 } = await supabase
   .from('leaderboard')
-  .upsert([{ id: userID, points: points }])
+  .upsert([{ id: userID, points: totalPoints }])
   .eq('id', userID)
   .select()
 
